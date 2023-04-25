4d4337ad2cf1513692d985af670a74972c13e290
noahe10 <118136389+noahe10@users.noreply.github.com>
2023-04-25 19:41:03 -0400

    Add files via upload
    Fixed the issue where when a user first opens the site, an error that states "http://127.0.0.1:8000/accounts/login/?next=/" does not exist. No matter what the user will first go to the login screen and in the event the user is already logged in, it will continue to the homepage

--- bug_app/urls.py
+++ bug_app/urls.py
@@ -4,10 +4,10 @@ from .views import appList, appDetail, appCreate, appUpdate, appDelete, login
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('login/', login.as_view(), name='login'),
+    path('', login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 
-    path('', appList.as_view(), name='appList'),
+    path('list/', appList.as_view(), name='appList'),
     path('detail/<int:pk>', appDetail.as_view(), name='appDetail'),
     path('create/', appCreate.as_view(), name='appCreate'),
     path('update/<int:pk>/', appUpdate.as_view(), name='appUpdate'),
